ce0c505081285098660497aaa242ba30b3c0ead9
Jannis Schreiber <jannis.schreiber@mobilityhouse.com>
2024-11-06 11:58:49 +0100

    refactor: extract tooling api queries

--- src/release-manifest/artifact-deploy-strategies/unlockedPackageInstallStep.ts
+++ src/release-manifest/artifact-deploy-strategies/unlockedPackageInstallStep.ts
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable class-methods-use-this */
 import { isEmpty } from '@salesforce/kit';
 import { Connection, Messages, SfError } from '@salesforce/core';
 import OclifUtils from '../../common/utils/wrapChildprocess.js';
 import { ZPackageInstallResultType } from '../../types/orgManifestOutputSchema.js';
 import { DeployStatus, DeployStrategies } from '../../types/orgManifestGlobalConstants.js';
 import { ZManifestOptionsType, ZUnlockedPackageArtifact } from '../../types/orgManifestInputSchema.js';
-import { Package2Version, SubscriberPackageVersion } from '../../types/sfToolingApiTypes.js';
+import {
+  PackageVersionDetails,
+  resolveInstalledVersionId,
+  resolvePackageVersionId,
+} from '../../common/metadata/toolingApiHelper.js';
 import { ArtifactDeployStrategy } from './artifactDeployStrategy.js';
 
 Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
@@ -46,17 +49,14 @@ export default class UnlockedPackageInstallStep implements ArtifactDeployStrateg
 
   public async resolve(targetOrg: Connection, devhubOrg: Connection): Promise<ZPackageInstallResultType> {
     this.internalState.targetUsername = targetOrg.getUsername();
-    const versionDetails = await this.resolvePackageVersionId(
+    const versionDetails = await resolvePackageVersionId(
       this.internalState.requestedVersion!,
       this.artifact.package_id,
       devhubOrg
     );
     this.assertInstallationKey(versionDetails);
     this.internalState.requestedVersionId = versionDetails.id;
-    const installedVersionDetails = await this.resolveInstalledVersionId(
-      versionDetails.subscriberPackageId!,
-      targetOrg
-    );
+    const installedVersionDetails = await resolveInstalledVersionId(versionDetails.subscriberPackageId!, targetOrg);
     this.internalState.installedVersionId = installedVersionDetails.id;
     this.internalState.installedVersion = installedVersionDetails.versionName;
     this.internalState.status = this.isResolved() ? DeployStatus.Enum.Resolved : DeployStatus.Enum.Skipped;
@@ -70,66 +70,6 @@ export default class UnlockedPackageInstallStep implements ArtifactDeployStrateg
       this.internalState.shouldSkipIfInstalled &&
       this.internalState.requestedVersionId === this.internalState.installedVersionId
     );
-  }
-
-  private async resolvePackageVersionId(
-    packageVersionLiteral: string,
-    packageId: string,
-    devhubCon: Connection
-  ): Promise<PackageVersionDetails> {
-    const versionArray = packageVersionLiteral.split('.');
-    const queryString = `SELECT 
-      SubscriberPackageVersionId,
-      Package2.SubscriberPackageId,
-      SubscriberPackageVersion.IsBeta,
-      SubscriberPackageVersion.IsPasswordProtected 
-      FROM Package2Version 
-      WHERE Package2Id = '${packageId}'
-        AND MajorVersion = ${versionArray[0]} 
-        AND MinorVersion = ${versionArray[1]} 
-        AND PatchVersion = ${versionArray[2]} 
-        AND IsReleased = true LIMIT 1`;
-    const queryResult = await devhubCon.tooling.query(queryString);
-    if (queryResult.records.length === 0) {
-      throw new SfError(
-        messages.getMessage('errors.no-released-package-version', [packageId, packageVersionLiteral]),
-        'NoReleasedPackageVersionFound'
-      );
-    }
-    const record = queryResult.records[0] as Package2Version;
-    return {
-      id: record.SubscriberPackageVersionId,
-      requiresInstallationKey: record.SubscriberPackageVersion.IsPasswordProtected,
-      subscriberPackageId: record.Package2.SubscriberPackageId,
-    };
-  }
-
-  private async resolveInstalledVersionId(
-    subscriberId: string,
-    targetOrgCon: Connection
-  ): Promise<PackageVersionDetails> {
-    const queryString = `SELECT
-      SubscriberPackageVersionId,
-      SubscriberPackageVersion.MajorVersion,
-      SubscriberPackageVersion.MinorVersion,
-      SubscriberPackageVersion.PatchVersion,
-      SubscriberPackageVersion.IsPasswordProtected
-      FROM InstalledSubscriberPackage
-      WHERE SubscriberPackageId = '${subscriberId}' LIMIT 1`;
-    const queryResult = await targetOrgCon.tooling.query(queryString);
-    if (queryResult.records.length === 0) {
-      return { id: undefined };
-    }
-    const record = queryResult.records[0] as Package2Version;
-    return {
-      id: record.SubscriberPackageVersionId,
-      versionName: this.mergeVersionName(record.SubscriberPackageVersion),
-      requiresInstallationKey: record.SubscriberPackageVersion.IsPasswordProtected,
-    };
-  }
-
-  private mergeVersionName(subscriberPackage: SubscriberPackageVersion): string {
-    return `${subscriberPackage.MajorVersion}.${subscriberPackage.MinorVersion}.${subscriberPackage.PatchVersion}`;
   }
 
   private assertInstallationKey(versionDetails: PackageVersionDetails): void {
@@ -169,10 +109,3 @@ export default class UnlockedPackageInstallStep implements ArtifactDeployStrateg
     return args;
   }
 }
-
-type PackageVersionDetails = {
-  id?: string;
-  versionName?: string;
-  requiresInstallationKey?: boolean;
-  subscriberPackageId?: string;
-};
