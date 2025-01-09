b8a3d52ba04d0196c20edf98ef88e9f77aee77e1
Jannis Schreiber <jannis.schreiber@mobilityhouse.com>
2025-01-09 14:35:28 +0100

    refactor: extract garbage collection api mocks

--- test/garbage-collection/garbageCollector.test.ts
+++ test/garbage-collection/garbageCollector.test.ts
@@ -1,7 +1,6 @@
-import fs from 'node:fs';
 import { expect } from 'chai';
 import { SinonStub } from 'sinon';
-import { QueryResult, Record } from '@jsforce/jsforce-node';
+import { Record } from '@jsforce/jsforce-node';
 import { Messages } from '@salesforce/core';
 import { TestContext, MockTestOrgData } from '@salesforce/core/testSetup';
 import GarbageCollector from '../../src/garbage-collection/garbageCollector.js';
@@ -11,22 +10,12 @@ import {
   ENTITY_DEFINITION_QUERY,
   PACKAGE_MEMBER_QUERY,
 } from '../../src/garbage-collection/queries.js';
-import {
-  DeveloperNamedRecord,
-  EntityDefinition,
-  FieldDefinition,
-  FlowVersionDefinition,
-  NamedRecord,
-  Package2,
-  Package2Member,
-  WorkflowAlertEntity,
-} from '../../src/types/sfToolingApiTypes.js';
+import { Package2Member } from '../../src/types/sfToolingApiTypes.js';
 import {
   loadSupportedMetadataTypes,
   loadUnsupportedMetadataTypes,
 } from '../../src/garbage-collection/entity-handlers/index.js';
-
-const testDataPath = 'test/garbage-collection/data';
+import GarbageCollectionMocks, { parseMockResult } from '../mock-utils/garbageCollectionMocks.js';
 
 Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
 const messages = Messages.loadMessages('@j-schreiber/sf-plugin', 'garbagecollection');
@@ -36,48 +25,24 @@ describe('garbage collector', () => {
   const testOrg = new MockTestOrgData();
   const devhubOrg = new MockTestOrgData();
 
+  let apiMocks: GarbageCollectionMocks;
   let fetchRecordsStub: SinonStub;
-  let PACKAGE_2: QueryResult<Package2>;
-  let PACKAGE_2_MEMBERS: QueryResult<Package2Member>;
-  let PACKAGED_FLOWS: QueryResult<Package2Member>;
-  let OBSOLETE_FLOW_VERSIONS: QueryResult<FlowVersionDefinition>;
-  let ENTITY_DEFINITIONS: QueryResult<EntityDefinition>;
-  let CUSTOM_LABELS: QueryResult<NamedRecord>;
-  let CUSTOM_OBJECT_ENTITY_DEFS: QueryResult<NamedRecord>;
-  let ALL_CUSTOM_FIELDS: QueryResult<FieldDefinition>;
-  let ALL_QUICK_ACTIONS: QueryResult<FieldDefinition>;
-  let ALL_LAYOUTS: QueryResult<FieldDefinition>;
-  let M00_CMDS: QueryResult<DeveloperNamedRecord>;
-  let M01_CMDS: QueryResult<DeveloperNamedRecord>;
-  let WORKFLOW_ALERTS: QueryResult<WorkflowAlertEntity>;
 
   beforeEach(async () => {
+    apiMocks = new GarbageCollectionMocks();
     testOrg.isDevHub = false;
     devhubOrg.isDevHub = true;
     await $$.stubAuths(testOrg, devhubOrg);
     fetchRecordsStub = $$.SANDBOX.stub(QueryRunner.prototype, 'fetchRecords');
     fetchRecordsStub.callsFake(fakeFetchRecords);
-    PACKAGE_2 = parseMockResult<Package2>('package-2.json');
-    PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/mixed.json');
-    PACKAGED_FLOWS = parseMockResult<Package2Member>('packaged-flows.json');
-    OBSOLETE_FLOW_VERSIONS = parseMockResult<FlowVersionDefinition>('outdated-flow-versions.json');
-    ENTITY_DEFINITIONS = parseMockResult<EntityDefinition>('entity-definitions.json');
-    CUSTOM_LABELS = parseMockResult<NamedRecord>('custom-labels.json');
-    CUSTOM_OBJECT_ENTITY_DEFS = parseMockResult<NamedRecord>('custom-object-entity-defs.json');
-    ALL_CUSTOM_FIELDS = parseMockResult<FieldDefinition>('all-custom-fields.json');
-    ALL_QUICK_ACTIONS = parseMockResult<FieldDefinition>('all-quick-actions.json');
-    ALL_LAYOUTS = parseMockResult<FieldDefinition>('layouts.json');
-    M00_CMDS = parseMockResult<FieldDefinition>('cmd-m00-records.json');
-    M01_CMDS = parseMockResult<FieldDefinition>('cmd-m01-records.json');
-    WORKFLOW_ALERTS = parseMockResult<WorkflowAlertEntity>('workflow-alert-definitions.json');
   });
 
   afterEach(() => {
     $$.SANDBOX.restore();
   });
 
-  function parseMockResult<T extends Record>(filePath: string) {
-    return JSON.parse(fs.readFileSync(`${testDataPath}/${filePath}`, 'utf8')) as QueryResult<T>;
+  function fakeFetchRecords<T extends Record>(queryString: string): Promise<Record[]> {
+    return apiMocks.fetchRecordsStub<T>(queryString);
   }
 
   it('registry loads all supported and unsupported handlers', async () => {
@@ -102,7 +67,7 @@ describe('garbage collector', () => {
 
   it('fetches package members and organizes results', async () => {
     // Arrange
-    OBSOLETE_FLOW_VERSIONS = { records: [], totalSize: 0, done: true };
+    apiMocks.OBSOLETE_FLOW_VERSIONS = { records: [], totalSize: 0, done: true };
 
     // Act
     const collector = new GarbageCollector(await testOrg.getConnection());
@@ -169,7 +134,7 @@ describe('garbage collector', () => {
 
   it('package members have custom field > resolves only undeleted custom field components', async () => {
     // Arrange
-    PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/custom-fields.json');
+    apiMocks.PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/custom-fields.json');
 
     // Act
     const collector = new GarbageCollector(await testOrg.getConnection());
@@ -189,7 +154,7 @@ describe('garbage collector', () => {
 
   it('package members have quick action > resolves quick action components', async () => {
     // Arrange
-    PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/quick-action.json');
+    apiMocks.PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/quick-action.json');
 
     // Act
     const collector = new GarbageCollector(await testOrg.getConnection());
@@ -207,7 +172,7 @@ describe('garbage collector', () => {
 
   it('package members have layouts > resolves layout components', async () => {
     // Arrange
-    PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/layouts.json');
+    apiMocks.PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/layouts.json');
 
     // Act
     const collector = new GarbageCollector(await testOrg.getConnection());
@@ -226,7 +191,7 @@ describe('garbage collector', () => {
 
   it('org has obsolete flow versions > includes each version', async () => {
     // Arrange
-    PACKAGE_2_MEMBERS = { records: [], totalSize: 0, done: true };
+    apiMocks.PACKAGE_2_MEMBERS = { records: [], totalSize: 0, done: true };
 
     // Act
     const collector = new GarbageCollector(await testOrg.getConnection());
@@ -250,7 +215,7 @@ describe('garbage collector', () => {
 
   it('has custom metadata types deprecated > includes custom metadata', async () => {
     // Arrange
-    PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/cmd-records.json');
+    apiMocks.PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/cmd-records.json');
 
     // Act
     const collector = new GarbageCollector(await testOrg.getConnection());
@@ -270,7 +235,7 @@ describe('garbage collector', () => {
 
   it('package members have workflow alerts > resolves entity def and alert name', async () => {
     // Arrange
-    PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/workflow-alerts.json');
+    apiMocks.PACKAGE_2_MEMBERS = parseMockResult<Package2Member>('package-members/workflow-alerts.json');
 
     // Act
     const collector = new GarbageCollector(await testOrg.getConnection());
@@ -356,7 +321,7 @@ describe('garbage collector', () => {
     // Arrange
     // package members are to subscriber id 0330X0000000000AAA
     // expect flows, where first flow is 0330X0000000000AAA, second flow is 033000000000001AAA
-    PACKAGE_2.records[0].SubscriberPackageId = '033000000000001AAA';
+    apiMocks.PACKAGE_2.records[0].SubscriberPackageId = '033000000000001AAA';
 
     // Act
     const collector = new GarbageCollector(await testOrg.getConnection(), await devhubOrg.getConnection());
@@ -371,47 +336,4 @@ describe('garbage collector', () => {
     expect(result.deprecatedMembers.Layout.components.length).to.equal(0);
     expect(result.deprecatedMembers.FlowDefinition.components.length).to.equal(3);
   });
-
-  function fakeFetchRecords<T extends Record>(queryString: string): Promise<Record[]> {
-    if (queryString.includes('FROM Package2 WHERE Id IN')) {
-      return Promise.resolve(PACKAGE_2.records);
-    }
-    if (queryString.includes('FROM Package2Member WHERE SubjectManageableState IN')) {
-      return Promise.resolve(PACKAGE_2_MEMBERS.records);
-    }
-    if (queryString.includes("FROM Package2Member WHERE SubjectKeyPrefix = '300'")) {
-      return Promise.resolve(PACKAGED_FLOWS.records);
-    }
-    if (queryString.includes('FROM EntityDefinition WHERE KeyPrefix IN')) {
-      return Promise.resolve(ENTITY_DEFINITIONS.records);
-    }
-    if (queryString.includes('FROM ExternalString WHERE Id IN')) {
-      return Promise.resolve(CUSTOM_LABELS.records);
-    }
-    if (queryString.includes("FROM EntityDefinition WHERE KeyPrefix LIKE 'a%'")) {
-      return Promise.resolve(CUSTOM_OBJECT_ENTITY_DEFS.records);
-    }
-    if (queryString.includes('FROM CustomField WHERE Id IN')) {
-      return Promise.resolve(ALL_CUSTOM_FIELDS.records);
-    }
-    if (queryString.includes('FROM QuickActionDefinition WHERE Id IN')) {
-      return Promise.resolve(ALL_QUICK_ACTIONS.records);
-    }
-    if (queryString.includes('FROM Layout WHERE Id IN')) {
-      return Promise.resolve(ALL_LAYOUTS.records);
-    }
-    if (queryString.includes("FROM Flow WHERE Status = 'Obsolete'")) {
-      return Promise.resolve(OBSOLETE_FLOW_VERSIONS.records);
-    }
-    if (queryString.includes('FROM CompanyData__mdt')) {
-      return Promise.resolve(M00_CMDS.records);
-    }
-    if (queryString.includes('FROM HandlerControl__mdt')) {
-      return Promise.resolve(M01_CMDS.records);
-    }
-    if (queryString.includes('FROM WorkflowAlert WHERE Id IN')) {
-      return Promise.resolve(WORKFLOW_ALERTS.records);
-    }
-    return Promise.resolve(new Array<T>());
-  }
 });
