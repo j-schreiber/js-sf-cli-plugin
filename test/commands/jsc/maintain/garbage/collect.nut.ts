630514a525bef72df2f4bc4a52bbf6871b7a7f8d
Jannis Schreiber <jannis.schreiber@mobilityhouse.com>
2025-01-09 10:27:43 +0100

    fix: output-dir param for nuts garbage collect

--- test/commands/jsc/maintain/garbage/collect.nut.ts
+++ test/commands/jsc/maintain/garbage/collect.nut.ts
@@ -53,7 +53,7 @@ describe('jsc data NUTs*', () => {
     it('collect garbage on fresh scratch org with package xml output', () => {
       // Act
       execCmd<PackageGarbageResult>(
-        `jsc:maintain:garbage:collect --target-org ${scratchOrgAlias} --output-format PackageXML --output-directory ${outputDir}`,
+        `jsc:maintain:garbage:collect --target-org ${scratchOrgAlias} --output-format PackageXML --output-dir ${outputDir}`,
         { ensureExitCode: 0 }
       );
 
